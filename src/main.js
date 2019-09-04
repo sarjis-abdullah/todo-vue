ca010f9f8ad322b6fc7fcec7584f6ab2795ffbdd
Sarjis <sarjis.m.a@gmail.com>
2019-09-04 12:47:01 +0600

    bootstrap package added

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
-
-Vue.use(BootstrapVue)
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'custom.scss'
+Vue.use(BootstrapVue)
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
