d6efaedec6ea48e885b7e2ab3f30cf76e436c088
Sarjis <sarjis.m.a@gmail.com>
2019-09-04 12:39:32 +0600

    initialize

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'custom.scss'
 Vue.config.productionTip = false
 
 new Vue({
